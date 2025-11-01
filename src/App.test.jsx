474d10c1b8f5366a644a0f7520e2000ea9cfe91c
John Moorman <45500240+mojoro@users.noreply.github.com>
2025-11-01 13:50:52 +0100

    Demonstrate test grouping with description

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,28 +14,28 @@ window.fetch = vi.fn(() => {
   });
 });
 
-test("loading text is shown while API request is in progress", async () => {
-  render(<App />);
-  const loading = screen.getByText("Loading...");
-
-  expect(loading).toBeInTheDocument();
-
-  await waitForElementToBeRemoved(() => screen.getByText("Loading..."));
-});
-
-test("user's name is rendered", async () => {
-  render(<App />);
-  const userName = await screen.findByText("Jack");
-  expect(userName).toBeInTheDocument();
-});
-
-test("error message is shown", async () => {
-  window.fetch.mockImplementationOnce(() => {
-    return Promise.reject({ message: "API is down" });
+describe("Testing App Component", () => {
+  test("loading text is shown while API request is in progress", async () => {
+    render(<App />);
+    const loading = screen.getByText("Loading...");
+    expect(loading).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.getByText("Loading..."));
   });
 
-  render(<App />);
+  test("user's name is rendered", async () => {
+    render(<App />);
+    const userName = await screen.findByText("Jack");
+    expect(userName).toBeInTheDocument();
+  });
 
-  const errorMessage = await screen.findByText("API is down");
-  expect(errorMessage).toBeInTheDocument();
+  test("error message is shown", async () => {
+    window.fetch.mockImplementationOnce(() => {
+      return Promise.reject({ message: "API is down" });
+    });
+
+    render(<App />);
+
+    const errorMessage = await screen.findByText("API is down");
+    expect(errorMessage).toBeInTheDocument();
+  });
 });
